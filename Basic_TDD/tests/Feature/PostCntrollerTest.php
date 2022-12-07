86d23a596eb34cb9c6500f2772e93657f8e600a4
Hasibul Islam Nirob <hasibolislamnirob@gmail.com>
2022-12-07 15:49:43 +0600

    show_list_of_post

--- Basic_TDD/tests/Feature/PostCntrollerTest.php
+++ Basic_TDD/tests/Feature/PostCntrollerTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\PostCntroller;
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -9,14 +11,21 @@ use Tests\TestCase;
 class PostCntrollerTest extends TestCase
 {
 
+    use RefreshDatabase;
+
     /** @test */
     public function show_list_of_post(){
         // Arrange / When
+        Post::factory()->count(10)->create();
 
         // Act / Given
+        $posts = (new PostCntroller)->index();
 
         // Assert / Then
-
+        $this->assertEquals(10, $posts->count());
 
     }
+
+
+
 }
